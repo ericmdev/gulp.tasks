2c8ac01884ed8a1abf30dd927083619a54661a14
Eric Mugerwa <ericmdev@gmail.com>
2015-12-27 18:54:29 +0000

    Add tasks/spec/jade.spec.js

---
+++ tasks/spec/jade.spec.js
@@ -0,0 +1,12 @@
+var jade = require('../jade.coffee');
+
+describe('Jade', function()
+{
+    'use strict';
+
+    it('not null', function()
+    {
+        expect(jade).not.toBe(null);
+    });
+    
+});
